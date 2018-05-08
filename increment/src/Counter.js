4ad130dc7dd660926905875b530c2ef793bd9d92
Shiv Raj Badu <shivrajbadu@gmail.com>
2018-05-08 10:47:22 +0545

    Reactapp increment is created and installed redux, react-redux library, defined initialState, created store and called counter component

---
+++ increment/src/Counter.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+function Counter(props) {
+  return (
+    <div>
+      <h1>Counter Application example</h1>
+      <p>Counter: {props.count}</p>
+      <button>Increment</button>
+    </div>
+  )
+}
+
+function mapStateToProps(state) {
+  console.log('mapStateToProps', state);
+
+  return {
+    count: state.count
+  }
+}
+
+// connect function creates a new component hooks mapStateToProps and our component 
+export default connect(mapStateToProps)(Counter);
+
+//export default Counter;//this is happening by connect function
---
+++ increment/src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore } from 'redux';
+
+// define initial state
+
+const initialState = {
+  count: 0
+}
+
+// create reducer function that takes two arguments state, action
+const reducer = (state=initialState, action) => {
+  console.log('reducer running', action);
+  return state;
+}
+
+// now create a store
+const store = createStore(reducer);
+
+export default store;
