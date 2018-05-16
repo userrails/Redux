8f44c22ea0453dcd790cfc64b605fb45eccc2537
Shiv Raj Badu <shivrajbadu@gmail.com>
2018-05-16 16:12:41 +0545

    return state to props in listings compoment by connect method

--- form-and-listing/src/components/posts/Listings.js
+++ form-and-listing/src/components/posts/Listings.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 
 class Listings extends React.Component {
   render () {
-    console.log('propstest-list', this.props.posts);
+    console.log('5.propstest-list', this.props.posts);
     if (this.props.posts) {
       return (
         <ul>
@@ -24,4 +24,9 @@ class Listings extends React.Component {
   }
 }
 
-export default Listings;
+const mapStateToProps = state => {
+  console.log('4. state-listings', state.posts)
+  return {posts: state.posts}
+}
+
+export default connect(mapStateToProps)(Listings);
