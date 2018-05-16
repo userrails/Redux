4b10a50df3a6d6bcca9257b6c7dda88bb08e6694
Shiv Raj Badu <shivrajbadu@gmail.com>
2018-05-16 14:38:47 +0545

    call the component form on the App component

--- form-and-listing/src/App.js
+++ form-and-listing/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import store from './store/posts/reducers';
+import Form from './components/posts/Form';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Fill out the form</h1>
+
+        <Form store={store} />
       </div>
     );
   }
