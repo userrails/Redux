bc4e95a3f46fc155e46f7e5c97cb6ab5740f7cef
Shiv Raj Badu <shivrajbadu@gmail.com>
2018-05-16 14:38:15 +0545

    code refactoring

--- form-and-listing/src/store/posts/reducers.js
+++ form-and-listing/src/store/posts/reducers.js
@@ -6,7 +6,7 @@ const initialState = {
 
 const rootReducer = (state=initialState, action) => {
   switch(action.type) {
-    case: 'ADD_POST':
+    case 'ADD_POST':
       return {...state, posts: [...state.posts, action.payload]};
     default:
       return state;
