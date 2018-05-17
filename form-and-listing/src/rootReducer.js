7aeb25d6b75662b36ac5dd173a5f82a54a1a94d4
Shiv Raj Badu <shivrajbadu@gmail.com>
2018-05-17 15:01:38 +0545

    refactoring

--- form-and-listing/src/rootReducer.js
+++ form-and-listing/src/rootReducer.js
@@ -3,7 +3,6 @@ import { combineReducers } from 'redux';
 import postReducer from './post/postReducer';
 import blogReducer from './blog/blogReducer';
 import categoryReducer from './category/categoryReducer';
-import { applyMiddleware } from 'redux';
 
 const rootReducer = combineReducers({
   postReducer,
