dfb304f12bdb4c112cd745bea01c62315ed27f8c
Shiv Raj Badu <shivrajbadu@gmail.com>
2018-05-17 19:41:27 +0545

    routes created

---
+++ form-and-listing/src/routes/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Home from '.././pages/Home';
+import Contact from '.././pages/Contact';
+import About from '.././pages/About';
+
+export default () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/contact" component={Contact} />
+      <Route path="/about" component={About} />
+    </Switch>
+  )
+}
