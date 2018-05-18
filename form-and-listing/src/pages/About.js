43ea5d247547c81d449ec409a8f533230cb1e3db
Shiv Raj Badu <shivrajbadu@gmail.com>
2018-05-18 10:04:17 +0545

    import jumbotron, grid, button from react-bootstrap && implemented on About component

--- form-and-listing/src/pages/About.js
+++ form-and-listing/src/pages/About.js
@@ -1,16 +1,30 @@
 import React, {Component} from 'react';
+import { Grid, Jumbotron, Button } from 'react-bootstrap';
 
 class About extends Component {
   render () {
     return (
       <div>
-        <h2>About Page</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <Jumbotron>
+          <Grid>
+            <h1>About Page</h1>
+            <p>
+              <Button
+                bsStyle="success"
+                bsSize="large"
+                href="http://react-bootstrap.github.io/components.html"
+                target="_blank">
+                View React Bootstrap Docs
+              </Button>
+            </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </Grid>
+        </Jumbotron>
       </div>
     )
   } 
