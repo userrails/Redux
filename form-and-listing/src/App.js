c451107ad4ee66a8ffa3e926555e3e69b0672dd2
Shiv Raj Badu <shivrajbadu@gmail.com>
2018-05-18 11:10:44 +0545

    Already created seperate pages so removed from this app component

--- form-and-listing/src/App.js
+++ form-and-listing/src/App.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import store from './configureStore';
-import PostForm from './post/PostForm';
-import PostList from './post/PostList';
-import BlogForm from './blog/BlogForm';
-import BlogList from './blog/BlogList';
-import CategoryForm from './category/CategoryForm';
-import CategoryList from './category/CategoryList';
 import Routes from './routes';
 import NavbarHeader from './app/NavbarHeader';
 import Footer from './app/Footer';
 
 class App extends Component {
   render() {
-  console.log("store", store)
     return (
       <div className="App">
         <NavbarHeader />
@@ -22,14 +14,6 @@ class App extends Component {
           <Routes />
         </div>
 
-        <h1>Fill out the form</h1>
-
-        <PostForm store={store} />
-        <PostList store={store} />
-        <BlogForm store={store} />
-        <BlogList store={store} />
-        <CategoryForm store={store} />
-        <CategoryList store={store} />
         <Footer />
       </div>
     );
