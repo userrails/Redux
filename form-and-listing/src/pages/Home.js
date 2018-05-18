77fdfb294d1e1aadc49fadadc2086dfb12877688
Shiv Raj Badu <shivrajbadu@gmail.com>
2018-05-18 09:58:50 +0545

    bootstrap css is working

--- form-and-listing/src/pages/Home.js
+++ form-and-listing/src/pages/Home.js
@@ -5,7 +5,7 @@ class Home extends Component {
   render () {
     return (
       <div>
-        <h2>Home Page</h2>
+        <h2 class="label label-info">Home Page</h2>
         <img src={Pic} alt="Logo"></img>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
