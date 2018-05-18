70ca462b6ae825c76477fce6dd0c7ed31f3971db
Shiv Raj Badu <shivrajbadu@gmail.com>
2018-05-18 10:55:55 +0545

    routes path for post defined, post has post form and post list

--- form-and-listing/src/routes/index.js
+++ form-and-listing/src/routes/index.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import Home from '.././pages/Home';
 import Contact from '.././pages/Contact';
 import About from '.././pages/About';
+import Post from '.././post/';
 
 export default () => {
   return (
@@ -11,6 +12,7 @@ export default () => {
       <Route exact path="/" component={Home} />
       <Route path="/contact" component={Contact} />
       <Route path="/about" component={About} />
+      <Route path="/post" component={Post} />
     </Switch>
   )
 }
