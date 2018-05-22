63fe872f46f57686929f995dd951cd86192fe1ac
Shiv Raj Badu <shivrajbadu@gmail.com>
2018-05-22 12:39:33 +0545

    userReducer has been added

--- form-and-listing/src/rootReducer.js
+++ form-and-listing/src/rootReducer.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import postReducer from './post/postReducer';
 import blogReducer from './blog/blogReducer';
 import categoryReducer from './category/categoryReducer';
+import userReducer from './user/userReducer';
 
 const rootReducer = combineReducers({
   postReducer,
   blogReducer,
-  categoryReducer
+  categoryReducer,
+  userReducer
 });
 
 export default rootReducer;
