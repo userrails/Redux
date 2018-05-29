1e5067fdef5f253a41fc2c58bc8481935f09b2e9
Shiv Raj Badu <shivrajbadu@gmail.com>
2018-05-29 15:45:32 +0545

    provider and store are imported and wrapped before the app

--- form-and-listing/src/index.js
+++ form-and-listing/src/index.js
@@ -6,10 +6,14 @@ import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import { Provider } from 'react-redux';
+import store from './configureStore';
 
 ReactDOM.render(
   <Router>
+    <Provider store={store}>
     <App />
+    </Provider>
   </Router>,
   document.getElementById('root')
 );
