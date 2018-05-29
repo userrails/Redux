c91d3644867b0a7bc5bca77a90db97e52998273f
Shiv Raj Badu <shivrajbadu@gmail.com>
2018-05-29 15:49:19 +0545

    removed unwanted code

--- form-and-listing/src/post_record/index.js
+++ form-and-listing/src/post_record/index.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 import PostRecordList from './PostRecordList';
-import store from '.././configureStore';
-import {Provider} from 'react-redux';
 
 class PostRecord extends Component {
   render () {
     return(
-      <Provider store={store}>
         <div>
           <PostRecordList />
         </div>
-      </Provider>
     )
   }
 }
